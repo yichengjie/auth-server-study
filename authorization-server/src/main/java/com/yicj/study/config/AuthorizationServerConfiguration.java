bf4d76a316f6e398e2b742af88484ed5ac4758ff
易成杰 <626659321@qq.com>
2020-05-16 11:31:35 +0800

    保存

--- authorization-server/src/main/java/com/yicj/study/config/AuthorizationServerConfiguration.java
+++ authorization-server/src/main/java/com/yicj/study/config/AuthorizationServerConfiguration.java
@@ -1,9 +1,12 @@
 package com.yicj.study.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 
@@ -11,18 +14,26 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 @EnableAuthorizationServer
 public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory().withClient("clientapp")
-                .secret("123456").authorizedGrantTypes("authorization_code")
+                .secret("123456").authorizedGrantTypes("authorization_code","password")
                 .redirectUris("http://localhost:9000/callback").scopes("read_users") ;
     }
-
 
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
         //配置了/oauth/check_token的访问权限为必须是认证过的用户才可以访问 -
         //AuthorizationServer里面配置/oauth/check_token的访问权限，默认是"denyAll"
         security.checkTokenAccess("isAuthenticated()") ;
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        //注意如果要使用密码模式，这里需要添加authenticationManager，否则报错说密码模式不可用
+        endpoints.authenticationManager(authenticationManager) ;
     }
 }
